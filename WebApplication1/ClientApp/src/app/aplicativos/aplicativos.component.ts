2d3ef67cdc9d0bd3f02e3847c46f8ad9be9b49f3
J.Mario Vieira <jm_bck@hotmail.com>
2020-12-13 19:56:14 -0300

    ok

--- WebApplication1/ClientApp/src/app/aplicativos/aplicativos.component.ts
+++ WebApplication1/ClientApp/src/app/aplicativos/aplicativos.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aplicativos.component.css']
 })
 export class AplicativosComponent implements OnInit {
-
+  apps:any =[];
   constructor() { }
 
   ngOnInit(): void {
+
+    this.monta_apps();
+
   }
 
+  monta_apps(){
+    debugger;
+    this.apps = sessionStorage.getItem('modulos');
+    console.log(JSON.stringify(this.apps));
+  }
 }
